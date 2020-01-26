d4fe7efeedb87fb82354c9a32f450fc8f20ec961
Tim Kao <pakuya.kao@gmail.com>
2020-01-26 10:47:50 -0800

    change file structures

--- pClass.cc
+++ pClass.cc
@@ -2,8 +2,8 @@
 #include "./GCC_4_7_0/1/Sales_item.h"
 
 int main() {
-  Sales_item book;
-  std::cin >> book;
-  std::cout << book << std::endl;
+  Sales_item item1, item2;
+  std::cin >> item1 >> item2;
+  std::cout << item1 + item2 << std::endl;
   return 0;
 }
