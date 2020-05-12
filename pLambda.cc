9252b4d8fe82ada229722fe5d956ef38dbd773f7
Tim Kao <pakuya.kao@gmail.com>
2020-05-11 19:40:41 -0700

    use for_each with lambda

--- pLambda.cc
+++ pLambda.cc
@@ -3,11 +3,19 @@
 #include <vector>
 
 // e10.14
-auto sumTwo = [](int a, int b) -> {
+auto sumTwo = [](int a, int b) -> int {
   return a + b;
 };
 
 int main() {
   std::cout << "write a lambda" << std::endl;
+  // e10.15
+  std::vector<int> nums = {1, 3, 5, 9, 11};
+  const int extra = 20;
+  for_each(nums.begin(), nums.end(), [&extra](int value) -> void {
+      int sum =  value + extra;
+      std::cout << sum << std::endl;
+  });
+
   return 0;
 }
