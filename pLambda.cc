b3ada1a507f424b251bb7f86e22ff55fca286540
Tim Kao <pakuya.kao@gmail.com>
2020-05-11 19:30:17 -0700

    simple lambda

---
+++ pLambda.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+// e10.14
+auto sumTwo = [](int a, int b) -> {
+  return a + b;
+};
+
+int main() {
+  std::cout << "write a lambda" << std::endl;
+  return 0;
+}
